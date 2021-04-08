7ccb6e78aa31bd7704582a20dbebd0238720c34e
reverieline@gmail.com <reverieline@gmail.com>
2021-04-08 19:33:06 +0000

    CICD: modify runner registrator

--- 03/02_register_runner.sh
+++ 03/02_register_runner.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-HOST=https://gitlab.com/
-TOKEN=FgftptvtUtRyYULryFT6
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <gitlab_host> <gitlab_runner_token>"
+    exit 1
+fi
+
+HOST="$1"
+TOKEN="$2"
 
 gitlab-runner register -n --url $HOST --registration-token $TOKEN --executor docker --description "Deployment Runner" --docker-image "docker:stable" --tag-list deployment --docker-privileged
