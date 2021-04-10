4c7fa1235a0c0b011cbcb6e513c186029e5b9879
Alex Gavr <reverieline@gmail.com>
2021-04-10 17:53:44 +0000

    CICD: runner registration taglist

--- 03/02_register_runner.sh
+++ 03/02_register_runner.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <gitlab_host> <gitlab_runner_token>"
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <gitlab_host> <gitlab_runner_token> <tag,list>"
     exit 1
 fi
 
 HOST="$1"
 TOKEN="$2"
+TAGLIST="$3"
 
-gitlab-runner register -n --url $HOST --registration-token $TOKEN --executor docker --description "Deployment Runner" --docker-image "docker:stable" --tag-list deployment --docker-privileged
+gitlab-runner register -n --url $HOST --registration-token $TOKEN --executor docker --description "Kora Runner" --docker-image "docker:stable" --tag-list $TAGLIST --docker-privileged
